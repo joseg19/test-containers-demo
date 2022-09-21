a060774b5c8a2925653af7224ce96a255478a352
Jose Guzman <2guzmanjose@gmail.com>
2022-09-21 20:17:47 -0300

    fix test

--- src/test/java/com/joseg/testcontainersdemo/UserIntegrationTest.java
+++ src/test/java/com/joseg/testcontainersdemo/UserIntegrationTest.java
@@ -1,40 +1,37 @@
 package com.joseg.testcontainersdemo;
 
 import com.joseg.testcontainersdemo.config.PostgresqlTestContainer;
-import org.junit.ClassRule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
-public class UserIntegrationTest {
+@ActiveProfiles(profiles = "test")
+@Testcontainers
+class UserIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @ClassRule
-    public static PostgreSQLContainer<PostgresqlTestContainer> postgreSQLContainer = PostgresqlTestContainer.getInstance();
+    @Container
+    private static final PostgreSQLContainer<PostgresqlTestContainer> postgreSQLContainer = PostgresqlTestContainer.getInstance();
 
     @Test
-    public void test() throws Exception {
+    void test() throws Exception {
         mockMvc.perform(get("/user/1"))
                 .andExpect(status().isOk())
                 .andExpect(content().json(""));
     }
-
-
 
 }
